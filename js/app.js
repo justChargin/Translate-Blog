1e19d68865418e5ad946a65b39c039090b02cfb6
Emir <newgds.27@gmail.com>
2020-05-08 09:29:59 +0300

    New Version

--- js/app.js
+++ js/app.js
@@ -1,51 +1,71 @@
-window.onload = () => {
-   let arrImg = document.querySelectorAll('.img-custom');
-
-    for(let i=0; i<arrImg.length; i++){
-        arrImg[i].setAttribute('width', '140px');
-        arrImg[i].setAttribute('height', '120px');
-        arrImg[i].classList.add('rounded-circle');
-    };
-
-/////////////////////////////////////////////////////////////////////
-    const main = document.querySelector('.container');
-    let sonuc = '';
-
-    function aRoute(user){
-        window.location.href = 'file:///C:/Users/Byte/Desktop/Git%20Files/AllFiles/Translate%20Blog/routes/' + user + '.html';
+class Users{
+    constructor(deneme){
+        this.main = document.querySelector('.container');
+        this.wrongPwd = document.querySelector('#wrongPwd');
+        this.wrongPwd.style.display = 'none';
     }
 
-    main.addEventListener('click', (e) => {
-        
-        const key = e.target.dataset.key;
-        if(key === 'userA'){
-            sonuc = key;
-        }else if(key === 'userB'){
-            sonuc = key;
-        }
+    allFunctions = () => {
+        this.listenEventsForUsers();
+        this.listenEventsForSubmit();
+        this.imageSize();
+    }
 
-        let psw = '';
-        if(key === 'submit'){
-            if(sonuc === 'userA'){
-                psw = document.querySelector('#pwd').value;
-                
-                if(psw === 'userAşifre'){
-                    console.log('Giriş Yapıldı.');
-                    aRoute(sonuc);
-                }else{
-                    console.log('Şifreniz Yanlış.');
-                }
-            }else if(sonuc === 'userB'){
-                psw = document.querySelector('#pwd').value;
-                if(psw === 'userBşifre'){
-                    aRoute(sonuc);
-                    console.log('Giriş Yapıldı.');
-                }else{
-                    console.log('Şifreniz Yanlış.');
-                }
+    imageSize = () => {
+        this.arrImg = document.querySelectorAll('.img-custom');
+        
+        for(let i=0; i<this.arrImg.length; i++){
+            this.arrImg[i].setAttribute('width', '140px');
+            this.arrImg[i].setAttribute('height', '120px');
+            this.arrImg[i].classList.add('rounded-circle');
+        };
+    }
+
+    control = (userName, password) => {
+        this.userName = userName;
+        this.password = password;
+        console.log('username: ' + this.userName + ' password: ' +this.password );
+    }
+
+    listenEventsForUsers = () => {
+        this.main.addEventListener('click', (e) => {
+            this.userKey = e.target.dataset.key;
+
+            if(this.userKey === 'userA'){
+                this.control(this.userKey, 'şifrea');
+            }else if(this.userKey === 'userB'){
+                this.control(this.userKey, 'şifreb');
+            }else{
+                return;
             }
-        }
-        
-    })
+        })
+    }
 
+    listenEventsForSubmit = () => {
+        this.main.addEventListener('click', (e) => {
+            this.submitKey = e.target.dataset.key;            
+            if(this.submitKey === 'submit'){
+                this.inputPassword = document.querySelector('#pwd').value;
+                this.wrongPwd = document.querySelector('#wrongPwd');
+
+                if(this.inputPassword === this.password){
+                    console.log(this.userName + ' logged in.');
+                    this.wrongPwd.style.display = 'none';
+
+                    document.querySelector('#pwd').value = '';
+                }else{
+                    this.wrongPwd.style.display = 'block'; //class ekleyip transition 3s
+
+                    document.querySelector('#pwd').value = '';
+                }
+            }else{
+                return;
+            }
+        })
+    }
+}
+
+window.onload = () => {
+    const user = new Users('oldu mu');
+    user.allFunctions();
 }
